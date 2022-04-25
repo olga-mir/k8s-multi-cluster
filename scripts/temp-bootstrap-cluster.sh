048ee4daa6df2403de4b3e410c25bb5db3fa7001
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2022-04-25 19:22:08 +1000

    Complete bootstrap and pivot process

--- scripts/temp-bootstrap-cluster.sh
+++ scripts/temp-bootstrap-cluster.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -eou pipefail
+workdir=$(pwd)
 
 # For more details please check docs/bootstrap-and-pivot.md doc in this repo
 
@@ -15,6 +16,10 @@ EOF
 
 kind create cluster --config bootstrap.yaml
 
+clusterctl init --infrastructure aws
+
+# Setup config environment variables, and AWS_B64ENCODED_CREDENTIALS
+# run `clusterctl generate cluster --list-variables aws` to get the list of variables
 if [ -z "$AWS_CONTROL_PLANE_MACHINE_TYPE" ] || \
    [ -z "$AWS_NODE_MACHINE_TYPE" ] || \
    [ -z "$AWS_SSH_KEY_NAME" ] || \
@@ -24,9 +29,51 @@ if [ -z "$AWS_CONTROL_PLANE_MACHINE_TYPE" ] || \
   exit 1
 fi
 
+set +e
+set -x
+while ! kubectl get clusters; do
+  sleep 15
+done
+set -e
+set +x
+echo \"No resources found in default namespace\" expected
+
+# deploy permanent mgmt cluster object in `default` ns in temp cluster
+# clusterctl generate cluster mgmt > mgmt.yaml
+
+kubectl apply -f $workdir/mgmt.yaml
+
+while ! kubectl wait kubeadmcontrolplane mgmt-control-plane --for jsonpath='{.status.initialized}'=true --timeout=30s; do
+  echo waiting for control plane to become initialized
+done
+
+clusterctl get kubeconfig mgmt > $workdir/target-mgmt.kubeconfig
+
+# backup previous kubeconfig - as necessary
+# cp $HOME/.kube/config $HOME/.kube/config-$(date +%F_%H_%M_%S)
+
+KUBECONFIG=$HOME/.kube/config:$workdir/kind.kubeconfig:$workdir/target-mgmt.kubeconfig kubectl config view --merge=true > $HOME/.kube/config
+
+##############
+############## ------ on AWS mgmt cluster ------
+##############
+
+kubectl config use-context mgmt-admin@mgmt
+
+kubectl apply -f https://docs.projectcalico.org/v3.21/manifests/calico.yaml
+#TODO https://projectcalico.docs.tigera.io/v3.22/manifests/calico.yaml
+
 clusterctl init --infrastructure aws
 
-# Setup config environment variables, and AWS_B64ENCODED_CREDENTIALS
-# run `clusterctl generate cluster --list-variables aws` to get the list of variables
+set +e
+set -x
+while ! kubectl get clusters; do
+  sleep 15
+done
+set -e
+set +x
+echo \"No resources found in default namespace\" expected
 
-clusterctl generate cluster mgmt > mgmt.yaml
+clusterctl move --to-kubeconfig=./target-mgmt.kubeconfig
+
+kind delete cluster
