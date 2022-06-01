40381e64f453d5b5c0d58e829840c494b1f9025e
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2022-06-01 18:41:43 +1000

    kubectl context rename and env vars for flux bootstrap parameters

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,10 @@ set -eou pipefail
 # Provide env vars and other settings in $workdir/mgmt-cluster/init-config-mgmt.yaml file
 # (note that the content of the file is not validated)
 # AWS_B64ENCODED_CREDENTIALS currently accepted only from env var only.
-if [ -z "$AWS_B64ENCODED_CREDENTIALS" ]; then
+if [ -z "$AWS_B64ENCODED_CREDENTIALS" ] && \
+   [ -z "$FLUX_REPO_SSH" ] && \   # ssh format for 'this' repo, e.g. ssh://git@github.com/olga-mir/k8s-multi-cluster
+   [ -z "$FLUX_BRANCH" ] && \     # branch in this repo for flux to sync
+   [ -z "$FLUX_KEY_PATH" ]; then  # path to ssh key which is a deployment key for this repo
   echo "Error AWS_B64ENCODED_CREDENTIALS is not provided" && exit 1
 fi
 
@@ -32,9 +35,8 @@ set +e
 while ! kubectl wait crd clusters.cluster.x-k8s.io --for=condition=Established --timeout=5s; do sleep 15; done
 set -e
 
-# deploy permanent mgmt cluster object in `default` ns in temp cluster
-# use manifests that were created before with `clusterctl generate cluster mgmt`
-# and committed to the repo
+# deploy permanent mgmt cluster object using
+# manifests that were created beforehand with `clusterctl generate cluster mgmt`
 
 # applying cluster manifests immediatelly will fail because components webhooks are not yet ready to serve traffic
 # it is easier to retry applying rather than checking on each component individually
@@ -59,7 +61,7 @@ set -e
 kubectl apply -f $workdir/mgmt-cluster/cm-calico-v3.21.yaml
 
 echo $(date '+%F %H:%M:%S')
-sleep 90
+sleep 120
 while ! kubectl wait --for condition=ResourcesApplied=True clusterresourceset crs-calico --timeout=30s ; do
   echo $(date '+%F %H:%M:%S') waiting for management cluster to become ready
 done
@@ -69,18 +71,15 @@ done
 
 # kubeconfig is available when this secret is ready: `k get secret mgmt-kubeconfig`
 clusterctl get kubeconfig mgmt > $workdir/target-mgmt.kubeconfig
-clusterctl init --infrastructure aws --kubeconfig $workdir/target-mgmt.kubeconfig --kubeconfig-context mgmt-admin@mgmt --config $workdir/mgmt-cluster/init-config-workload.yaml
+# Apart from being shorter and nicer, it is also required later for kubefed which breaks when there are special chars in context name
+kubectl config rename-context mgmt-admin@mgmt mgmt
+
+clusterctl init --infrastructure aws --kubeconfig $workdir/target-mgmt.kubeconfig --kubeconfig-context mgmt --config $workdir/mgmt-cluster/init-config-workload.yaml
 
 echo $(date '+%F %H:%M:%S')
 set +e
-# this is confusing wait. At this stage we don't expect to see clusters in the mgmt cluster
-# this is only checking that clusters *can* be created, i.e. that clusterctl init worked.
-# there should be better check
-while ! kubectl --kubeconfig=$workdir/target-mgmt.kubeconfig get clusters; do
-  sleep 15
-done
+while ! kubectl --kubeconfig=$workdir/target-mgmt.kubeconfig wait crd clusters.cluster.x-k8s.io --for=condition=Established; do sleep 15; done
 set -e
-echo \"No resources found in default namespace\" expected
 
 clusterctl move --to-kubeconfig=./target-mgmt.kubeconfig
 
@@ -109,10 +108,10 @@ clusterctl move --to-kubeconfig=./target-mgmt.kubeconfig
 # `k get secret flux-system -n flux-system` is the secret in the link above (ssh-credentials)
 
 flux bootstrap git \
-  --kubeconfig=$workdir/target-mgmt.kubeconfig --context mgmt-admin@mgmt \
-  --url=ssh://git@github.com/olga-mir/k8s-multi-cluster \
-  --branch=feature/kubefed-and-kong \
-  --private-key-file=$HOME/.ssh/flux-github-key \
+  --kubeconfig=$workdir/target-mgmt.kubeconfig --context mgmt \
+  --url=$FLUX_REPO_SSH \
+  --branch=$FLUX_BRANCH \
+  --private-key-file=$FLUX_KEY_PATH \
   --path=clusters/mgmt
 
 
@@ -120,16 +119,17 @@ flux bootstrap git \
 
 # Once Flux is bootstrapped on the cluster it will apply cluster-dev CAPI definition and workload cluster will start provisioning
 sleep 90
-while ! kubectl --kubeconfig=$workdir/target-mgmt.kubeconfig wait --context mgmt-admin@mgmt --for condition=ResourcesApplied=True clusterresourceset crs-calico -n cluster-dev --timeout=15s ; do
+while ! kubectl --kubeconfig=$workdir/target-mgmt.kubeconfig wait --context mgmt --for condition=ResourcesApplied=True clusterresourceset crs-calico -n cluster-dev --timeout=15s ; do
   echo $(date '+%F %H:%M:%S') waiting for workload cluster to become ready
   sleep 15
 done
 
-clusterctl --kubeconfig=$workdir/target-mgmt.kubeconfig --kubeconfig-context mgmt-admin@mgmt get kubeconfig dev -n cluster-dev > $workdir/dev.kubeconfig
+clusterctl --kubeconfig=$workdir/target-mgmt.kubeconfig --kubeconfig-context mgmt get kubeconfig dev -n cluster-dev > $workdir/dev.kubeconfig
+kubectl config rename-context dev-admin@dev dev
 
 flux bootstrap git \
-  --kubeconfig=$workdir/dev.kubeconfig --context dev-admin@dev \
-  --url=ssh://git@github.com/olga-mir/k8s-multi-cluster \
-  --branch=feature/kubefed-and-kong \
-  --private-key-file=$HOME/.ssh/flux-github-key \
+  --kubeconfig=$workdir/dev.kubeconfig --context dev \
+  --url=$FLUX_REPO_SSH \
+  --branch=$FLUX_BRANCH \
+  --private-key-file=$FLUX_KEY_PATH \
   --path=clusters/cluster-dev
