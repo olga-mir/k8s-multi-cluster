087d9d9923de0c911db828908c164bfae11a565d
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2022-06-01 21:40:58 +1000

    Cleanup improvement: move clusters back to kind and delete in parallel

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,6 +4,10 @@ workdir=$(pwd)
 
 # This script assumes it runs on the same setup as deployed by other scripts in this repo.
 # The following contexts are expected to be available in kubeconfig (use ./scripts/merge-kubeconfig.sh) to merge all in one
+# CURRENT   NAME            CLUSTER     AUTHINFO     NAMESPACE
+# *         dev             dev         dev-admin
+#           kind-kind       kind-kind   kind-kind
+#           mgmt            mgmt        mgmt-admin
 
 if [[ -z "$ACCEPT_CLEANUP_T_AND_C" ]]; then
   echo "ERROR"
@@ -19,13 +23,24 @@ echo Suspend CAPI sources reconciliation.
 flux suspend kustomization infrastructure
 
 echo $(date '+%F %H:%M:%S')
-kubectl delete cluster dev -n cluster-dev
-
-echo $(date '+%F %H:%M:%S')
-#clusterctl move --kubeconfig=$workdir/target-mgmt.kubeconfig --kubeconfig-context mgmt-admin@mgmt --to-kubeconfig=$HOME/.kube/config --to-kubeconfig-context=kind-kind
-clusterctl move --to-kubeconfig=$HOME/.kube/config --to-kubeconfig-context=kind-kind
+clusterctl move --to-kubeconfig=$HOME/.kube/config --to-kubeconfig-context=kind-kind -n cluster-dev
+clusterctl move --to-kubeconfig=$HOME/.kube/config --to-kubeconfig-context=kind-kind -n cluster-mgmt
 
 echo "---- Switching to 'kind' cluster"
 kubectl config use-context kind-kind
-# TODO - test: move both clusters to `kind` and delete in parallel
-# kubectl delete cluster mgmt -n cluster-mgmt
+kubectl delete cluster mgmt -n cluster-mgmt &
+kubectl delete cluster dev -n cluster-dev &
+
+echo $(date '+%F %H:%M:%S')
+sleep 600
+
+while kubectl get cluster mgmt -n cluster-mgmt; do
+  sleep 60
+done
+
+echo $(date '+%F %H:%M:%S')
+while kubectl get cluster dev -n cluster-dev; do
+  sleep 30
+done
+
+kind delete cluster
