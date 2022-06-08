125220bea9cce0266f8aec6c6ba70a4ffe54dc9f
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2022-06-08 19:43:29 +1000

    Add flux CRS resources for mgmt and workload clusters

--- scripts/pre-fetch.sh
+++ scripts/pre-fetch.sh
@@ -17,37 +17,46 @@ main() {
 
   # disable Hubble because otherwise the manifest contains secrets with CAs and private keys.
   # I don't understand this now and I don't want to commit clear text secret manifests or deal with secrets right now.
-  helm template cilium cilium/cilium --version $CILIUM_VERSION --namespace kube-system --set hubble.enabled=false > $temp_dir/cilium-${CILIUM_VERSION}.yaml
+  helm template cilium cilium/cilium --version $CILIUM_VERSION --namespace kube-system --set hubble.enabled=false > $temp_dir/cilium-$CILIUM_VERSION.yaml
 
-  kubectl create configmap crs-cm-cilium-${CILIUM_VERSION} --from-file=$temp_dir/cilium-${CILIUM_VERSION}.yaml --dry-run=client -o yaml > $workdir/infrastructure/base/crs-cm-cilium-${CILIUM_VERSION}.yaml
+  kubectl create configmap crs-cm-cilium-$CILIUM_VERSION --from-file=$temp_dir/cilium-$CILIUM_VERSION.yaml --dry-run=client -o yaml > $workdir/infrastructure/base/crs-cm-cilium-$CILIUM_VERSION.yaml
 
   ## Flux
   # This script can be used for upgrading flux, not only installing it, so
   # flux-system folder and the gotk-sync files are created outside of this script
-  flux install --version=$FLUXCD_VERSION --export > $temp_dir/gotk-components.yaml
+  flux install --version=$FLUXCD_VERSION --export > $temp_dir/gotk-components-$FLUXCD_VERSION.yaml
 
-  cp $temp_dir/gotk-components.yaml $workdir/clusters/tmp-mgmt/flux-system
-  cp $temp_dir/gotk-components.yaml $workdir/clusters/staging/mgmt/flux-system
-  cp $temp_dir/gotk-components.yaml $workdir/clusters/staging/blue/flux-system
+  #cp $temp_dir/gotk-components.yaml $workdir/clusters/tmp-mgmt/flux-system
+  #cp $temp_dir/gotk-components.yaml $workdir/clusters/staging/mgmt/flux-system
+  #cp $temp_dir/gotk-components.yaml $workdir/clusters/staging/blue/flux-system
 
   # tmp management cluster is the only one that gets flux as gotk-components manifests
   # all other clusters are created by CAPI and will have flux manifests packaged inside a CRS
 
   # ok, this feels silly, but this is how I understand it now:
   # flux is running in read-only mode (by choice. can't use bootstrap if installing via CRS?)
-  # but it syncs from a repo which needs to include flux-system too.
+  # but flux-system ns with gotk-*.yaml manifests needs to be present in the repo.
   # so flux manifests are stored twice - once as plain manifest at the final state path
   # and once as a payload inside a configmap for the CRS.
 
-  flux_yaml=$temp_dir/flux-combined.yaml
-  cp $workdir/clusters/staging/mgmt/flux-system/gotk-components.yaml $flux_yaml
+  flux_yaml=$temp_dir/flux-combined.yaml # test - not ure if CRS can apply CRD and CR in the same CRS
+  cp  $temp_dir/gotk-components-$FLUXCD_VERSION.yaml $flux_yaml
   echo "---" >> $flux_yaml
   cat $workdir/clusters/staging/mgmt/flux-system/gotk-sync.yaml >> $flux_yaml
 
   # now we can put this in CM. (k create cm accepts --from-<whatever> multiple times,
   # but it creates a separate data entry for each occurence, that's why concatenating file was necessary
-  kubectl create configmap crs-cm-flux-${FLUXCD_VERSION}-mgmt --from-file=$flux_yaml -n cluster-mgmt --dry-run=client -o yaml > $workdir/infrastructure/base/crs-cm-flux-mgmt.yaml
-  # (this should not be base path - cluster specific things are baked into CM payload and are not accessible to kustomize. facepalm emoji goes here)
+  kubectl create configmap crs-cm-flux-${FLUXCD_VERSION}-mgmt --from-file=$flux_yaml -n cluster-mgmt --dry-run=client -o yaml > $workdir/infrastructure/tmp-mgmt/crs-cm-flux-${FLUXCD_VERSION}-mgmt.yaml
+
+  # rinse, repeat. see if it works first, then improve.
+  > $temp_dir/flux-combined.yaml
+  flux_yaml=$temp_dir/flux-combined.yaml
+  cp  $temp_dir/gotk-components-$FLUXCD_VERSION.yaml $flux_yaml
+  echo "---" >> $flux_yaml
+  cat $workdir/clusters/staging/blue/flux-system/gotk-sync.yaml >> $flux_yaml
+
+  kubectl create configmap crs-cm-flux-${FLUXCD_VERSION}-workload --from-file=$flux_yaml -n cluster-dev --dry-run=client -o yaml > $workdir/infrastructure/control-plane-cluster/capi/crs-cm-flux-${FLUXCD_VERSION}-workload.yaml
+
 }
 
 cleanup() {
