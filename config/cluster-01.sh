8c63e6df8c303b4c31faf86ec04fce7d2b7cdce6
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2022-07-03 12:27:49 +1000

    feat: re-org CAPI and Cilium config

---
+++ config/cluster-01.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# see <repo_root>/config/README.md
+
+export CLUSTER_NAME="cluster-01"
+export POD_CIDR="192.168.0.0/20"
+
+# CAPI variables
+export KUBERNETES_VERSION="1.23.8"
+export AWS_CONTROL_PLANE_MACHINE_TYPE="t3.medium"
+export AWS_NODE_MACHINE_TYPE="t3.medium"
+export CONTROL_PLANE_MACHINE_COUNT="1"
+export WORKER_MACHINE_COUNT="1"
+export EXP_CLUSTER_RESOURCE_SET="true"
+
+# CNI variables
+export CILIUM_VERSION="1.11.6"
+export NODE_MASK_SIZE="24"
+export CLUSTER_INT_ID=1
