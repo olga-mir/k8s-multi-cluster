1102f5af9fa2cb746e1a1dbf8f4a7bef43f6d992
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2022-07-03 12:27:49 +1000

    feat: re-org CAPI and Cilium config

---
+++ config/cluster-mgmt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# see <repo_root>/config/README.md
+
+# Common
+export CLUSTER_NAME="mgmt"
+export POD_CIDR="192.168.0.0/20"
+
+# CAPI variables
+export KUBERNETES_VERSION="1.23.8"
+export AWS_CONTROL_PLANE_MACHINE_TYPE="t3.medium"
+export AWS_NODE_MACHINE_TYPE="t3.medium"
+export CONTROL_PLANE_MACHINE_COUNT="1"
+export WORKER_MACHINE_COUNT="1"
+export EXP_CLUSTER_RESOURCE_SET="true"
+
+
+# CNI variables
+export CILIUM_VERSION="1.11.6"
+export NODE_MASK_SIZE="24"
+# mgmt cluster is not part of cluster mesh
+unset CLUSTER_INT_ID
