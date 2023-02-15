5c9228e41341df0f2fc9b9915256c8002525c118
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2023-02-16 07:43:26 +1100

    regenerate CAAPH manifests

--- scripts/generate-caaph-manifests.sh
+++ scripts/generate-caaph-manifests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -eoux pipefail
 
-REPO_ROOT=$(git rev-parse --show-toplevel)
+echo "MUST RUN FROM kubernetes-sigs/cluster-api-addon-provider-helm REPOSITORY!"
 
 REGISTRY=$1
 IMG=$2
@@ -8,10 +9,10 @@ IMG=$2
 MANIFESTS_FILE="manifests.yaml"
 
 cat > $MANIFESTS_FILE << EOF
-# This manifests are generated from
+# This manifest is generated from https://github.com/kubernetes-sigs/cluster-api-addon-provider-helm
 # Apache License 2.0: https://github.com/kubernetes-sigs/cluster-api-addon-provider-helm/blob/main/LICENSE
+# You must regenerate to use your own image
 EOF
-
 echo "---" >> $MANIFESTS_FILE
 cd config/manager && kustomize edit set image controller=${REGISTRY}/${IMG}
 cd ../../ && kustomize build config/default >> $MANIFESTS_FILE
